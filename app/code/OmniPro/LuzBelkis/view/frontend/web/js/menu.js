490f5ace7a73fda68dbb71bb076b1c2dd0ef2e90
lfuentes-omnipro <luz.fuentes@omni.pro>
2021-04-26 04:22:24 +0000

    nuevo modulo luz belkis

---
+++ app/code/OmniPro/LuzBelkis/view/frontend/web/js/menu.js
@@ -0,0 +1,12 @@
+requestAnimationFrame([
+    'jquery',
+    'acordion'
+])function($){
+$(document).ready(function(){
+    var tab =1;
+    $("#tab-login");
+    $("#register").hide();
+    $("#help").hide();
+    
+}
+}
