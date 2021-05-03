9b5811bb6a83f9b091e25138321c2fbacbe51e8f
lfuentes-omnipro <luz.fuentes@omni.pro>
2021-05-03 04:46:27 +0000

    change

--- app/code/OmniPro/LuzBelkis/view/frontend/web/js/menu.js
+++ app/code/OmniPro/LuzBelkis/view/frontend/web/js/menu.js
@@ -1,12 +1,29 @@
-requestAnimationFrame([
-    'jquery',
-    'acordion'
-])function($){
-$(document).ready(function(){
-    var tab =1;
-    $("#tab-login");
-    $("#register").hide();
-    $("#help").hide();
-    
-}
-}
+    require(['jquery'], function($){ 
+        $( document ).ready(function() {
+            // alert("primer alerta");
+            $( ".auth__login-register .nav-tabs .nav-item " ).on( "click", function() {
+   
+                if($(this).hasClass("login")){
+                    $(this).find('a').addClass("active");
+                    $(".auth__login-register .nav-tabs .nav-item.register a ").removeClass("active");
+                    $(".tab-content #login").addClass("active");
+                    $(".tab-content #register").removeClass("active");
+                }
+                else if($(this).hasClass("register")){
+                    $(this).find('a').addClass("active");
+                    $(".auth__login-register .nav-tabs .nav-item.login a ").removeClass("active");
+                    $(".tab-content #register").addClass("active");
+                    $(".tab-content #login").removeClass("active");
+                }
+            });
+
+            $('.auth__menu-ico').click(function() {
+                $( ".sidenav-menu.bg-menu" ).toggleClass("active");
+            });
+            $('.closebtn').click(function() {
+                $( ".sidenav-menu.bg-menu" ).toggleClass("active");
+            });
+            
+        });    
+    });
+
